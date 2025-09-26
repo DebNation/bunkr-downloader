d86d1d23706e9ec1074956156af6aaba82f5d653
Debiprasad Das <debiprasad@proton.me>
2025-09-26 18:11:19 +0530

    use URLS.txt file to paste links

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import subprocess
 import urllib.parse
 from downloader import download_file_with_progress
 
-url = input("Enter a url: ")
-result = subprocess.run(["node", "index.js", url], capture_output=True, text=True)
-filename = result.stdout.split("=")[-1].strip()
-decoded_filename = urllib.parse.unquote(filename)
-download_file_with_progress(result.stdout.strip(), decoded_filename)
+# url = input("Enter a url: ")
+with open("URLS.txt", "r") as file:
+    for line in file:
+        if line.strip() == "":
+            print("No URL given!")
+            continue
+        result = subprocess.run(
+            ["node", "index.js", line.strip()], capture_output=True, text=True
+        )
+        filename = result.stdout.split("=")[-1].strip()
+        decoded_filename = urllib.parse.unquote(filename)
+        download_file_with_progress(result.stdout.strip(), decoded_filename)
